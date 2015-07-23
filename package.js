a873bc15e5da80f44da4f9f86e2f010b70b8486d
Rob Lingle <rob@oak.works>
2015-07-23 10:51:04 -0400

    server-side aggregates: rough, abandoning for now

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "A reactive table designed for Meteor",
-  version: "0.8.11",
-  name: "aslagle:reactive-table",
+  version: "0.8.12",
+  name: "rlingle:reactive-table",
   git: "https://github.com/aslagle/reactive-table.git"
 });
 
@@ -55,12 +55,12 @@ Package.on_test(function (api) {
     api.add_files('test/test_settings.js', 'client');
     api.add_files('test/test_fields_tmpl.html', 'client');
     api.add_files('test/test_fields.js', 'client');
-  
+
     api.use('accounts-password@1.0.6', ['client', 'server']);
     api.add_files('test/test_reactivity_server.js', 'server');
     api.add_files('test/test_reactivity.html', 'client');
     api.add_files('test/test_reactivity.js', 'client');
-  
+
     api.add_files('test/test_sorting.js', 'client');
     api.add_files('test/test_filtering_server.js', 'server');
     api.add_files('test/test_filtering.js', 'client');
